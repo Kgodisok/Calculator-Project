795c167e03d4312f02f7bfc168d09d0b48b49f50
Matsepe Kgodiso <kgodisomatsepe474@mail.com>
2026-05-18 07:49:07 +0200

    Initial commit of react app

---
+++ portmat-accommodation-frontend/portmat-accommodation/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Portmat Accommodation</h1>
+    </div>
+  )
+}
+
+export default App
